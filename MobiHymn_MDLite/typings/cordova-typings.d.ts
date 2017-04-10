0ec8e1d50de35e03e5f7c925d0a7d2f915040178
Timothy John <tim.gandionco@gmail.com>
2017-04-10 23:30:11 +0800

    rebuild for ios and android

--- MobiHymn_MDLite/typings/cordova-typings.d.ts
+++ MobiHymn_MDLite/typings/cordova-typings.d.ts
@@ -1,3 +1,7 @@
 
 /// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
 /// <reference path="../.vscode/typings/cordova/plugins/StatusBar.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Media.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/FileSystem.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Device.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Splashscreen.d.ts"/>
